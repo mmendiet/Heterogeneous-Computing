5125e3b01486287fcf80dd6cddb5cdd39c99e13c
Matias <mmendiet@uncc.edu>
2020-02-06 14:35:13 -0500

    Added CPU code for vector add

--- HW1/testing.cu
+++ HW1/testing.cu
@@ -4,15 +4,24 @@
 
 #include <iostream>
 #include <fstream>
+#include <chrono>
 
 //#define N 1000
 //#define M 512
+//nvcc testing.cu -o test
+//
 
 
 __global__ void add(int *a, int *b, int *c, int n) {
     int index = threadIdx.x + blockIdx.x * blockDim.x;
     if (index < n)
         c[index] = a[index] + b[index];
+}
+
+void cpuAdd(int *a, int *b, int *c, int n) {
+    for(int i=0; i<n; ++i) {
+        c[0] = a[0] + b[0];
+    }
 }
 
 void random_ints(int* x, int size)
@@ -27,41 +36,62 @@ int main(int argc, char* argv[]) {
     int N = atoi(argv[1]);
     int M = atoi(argv[2]);
     int *a, *b, *c;
-    // host copies of a, b, c
-    int *d_a, *d_b, *d_c;
+
     // device copies of a, b, c
     int size = N * sizeof(int);
-    // Allocate space for device copies of a, b, c
-    cudaMalloc((void **)&d_a, size);
-    cudaMalloc((void **)&d_b, size);
-    cudaMalloc((void **)&d_c, size);
+
     // Setup input values
     a = (int*)malloc(size); random_ints(a, N);
     b = (int*)malloc(size); random_ints(b, N);
-    c = (int*)malloc(size);       
+    c = (int*)malloc(size);  
+    if (strcmp(argv[3],"gpu")==0) {
+        // host copies of a, b, c
+        int *d_a, *d_b, *d_c;
+        // Allocate space for device copies of a, b, c
+        cudaMalloc((void **)&d_a, size);
+        cudaMalloc((void **)&d_b, size);
+        cudaMalloc((void **)&d_c, size);
 
-    // Copy inputs to device
-    cudaMemcpy(d_a, a, size, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_b, b, size, cudaMemcpyHostToDevice);
-    // Launch add() kernel on GPU
-    cudaEvent_t start, stop;
-    float time;
-    cudaEventCreate(&start);
-    cudaEventCreate(&stop);
-    cudaEventRecord( start, 0 );
-    add<<<(N + M-1) / M,M>>>(d_a, d_b, d_c, N);
-    cudaEventRecord( stop, 0 );
-    cudaEventSynchronize( stop );
+        // Copy inputs to device
+        cudaMemcpy(d_a, a, size, cudaMemcpyHostToDevice);
+        cudaMemcpy(d_b, b, size, cudaMemcpyHostToDevice);
+        // Launch add() kernel on GPU
+        float total_time = 0;
+        for(int i=0; i< 1000; ++i) {
+            float time;
+            cudaEvent_t start, stop;
+            
+            cudaEventCreate(&start);
+            cudaEventCreate(&stop);
+            cudaEventRecord( start, 0 );
+            add<<<(N + M-1) / M,M>>>(d_a, d_b, d_c, N);
+            cudaEventRecord( stop, 0 );
+            cudaEventSynchronize( stop );
 
-    cudaEventElapsedTime( &time, start, stop );
-    cudaEventDestroy( start );
-    cudaEventDestroy( stop );
-    // Copy result back to host
-    cudaMemcpy(c, d_c, size, cudaMemcpyDeviceToHost);
+            cudaEventElapsedTime( &time, start, stop );
+            cudaEventDestroy( start );
+            cudaEventDestroy( stop );
+            total_time += time;
+        }
+        // Copy result back to host
+        cudaMemcpy(c, d_c, size, cudaMemcpyDeviceToHost);
+        cudaFree(d_a); cudaFree(d_b); cudaFree(d_c);
+        float nanosec = (total_time);
+        std::cout << "N: " << N << "   M: " << M << "   GPU time: " << nanosec << "ns" << std::endl;
+    }
+
+    else {
+        auto t1 = std::chrono::high_resolution_clock::now();
+        cpuAdd(a,b,c, N);
+        auto t2 = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>( t2 - t1 ).count();
+        std::cout << "N: " << N << "   M: " << M << "   CPU time: " << duration << "ns" << std::endl;
+        //time = 100.0;
+    }
     //printf("a[0]: %i, b[0]: %i, c[0]: %i\nGPU Time: %f\n", a[0], b[0], c[0], time);
-    printf("N: %i   M: %i   Time: %f\n", N, M, time);
+    //printf("N: %i   M: %i   Time: %f\n", N, M, time);
     // Cleanup
     free(a); free(b); free(c);
-    cudaFree(d_a); cudaFree(d_b); cudaFree(d_c);
+
     return 0;
 }
--- HW1/tests.sh
+++ HW1/tests.sh
@@ -2,6 +2,7 @@ for N in 1000 10000 100000 1000000 10000000
     do
         for M in 128 256 512 1024
             do
-                ./test $N $M
+                ./test $N $M gpu
+                #./test $N $M cpu
             done
     done
